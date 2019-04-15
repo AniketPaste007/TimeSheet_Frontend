a98f53e4ba431bbf4f371d8f1c6e23237faceb11
AniketPaste007 <pasteaniket111@gmail.com>
2019-04-15 17:11:54 +0530

    user dropdown

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Button, Radio, DatePicker, message, Alert } from "antd";
+import {
+  Button,
+  Radio,
+  DatePicker,
+  message,
+  Menu,
+  Icon,
+  Dropdown,
+  Alert
+} from "antd";
 import "antd/dist/antd.css";
 import "./index.css";
 import EditableFormTable from "./table";
@@ -53,15 +62,48 @@ class App extends React.Component {
     console.log(this.state.date);
   }
 
+  handleButtonClick = e => {
+    message.info("Click on left button.");
+    console.log("click left button", e);
+  };
+
+  handleMenuClick = e => {
+    message.info("Click on menu item.");
+    console.log("click", e);
+  };
+
   render() {
     const { date } = this.state;
     var shown = {
       display: this.state.shown ? "block" : "none"
     };
 
+    const menu = (
+      <Menu onClick={this.handleMenuClick}>
+        <Menu.Item key="1">
+          <Icon type="user" />
+          Kajol
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Icon type="user" />
+          Vaibhav
+        </Menu.Item>
+        <Menu.Item key="3">
+          <Icon type="user" />
+          Kiran
+        </Menu.Item>
+      </Menu>
+    );
+
     return (
       <div align="center" style={{ width: "80%", margin: "50px auto" }}>
         <h1>TIMESHEET ENTRY</h1>
+        <div>
+          <Dropdown.Button onClick={this.handleButtonClick} overlay={menu}>
+            Select User
+          </Dropdown.Button>
+        </div>
+        <br />
         <div>
           <RadioGroup onChange={this.onchange} value={this.state.value}>
             <Radio value={1} onChange={this.toggletoday.bind(this)}>
